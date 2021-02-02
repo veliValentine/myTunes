8b48ef6d07fef0c194ac805e4e7700c53821d1b5
veliValentine <nikkevanukas1995@gmail.com>
2021-02-02 16:47:44 +0200

    feat: init

---
+++ src/main/java/group/demo/MyTunes.java
@@ -0,0 +1,12 @@
+package group.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTunes {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTunes.class, args);
+    }
+}
