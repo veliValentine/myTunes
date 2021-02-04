5f11d3ecd360e58474509bd77beb590a35d6a273
veliValentine <nikkevanukas1995@gmail.com>
2021-02-04 13:22:03 +0200

    refactor: changed base URI name

--- src/main/java/group/demo/controllers/ControllerHelper.java
+++ src/main/java/group/demo/controllers/ControllerHelper.java
@@ -1,5 +1,5 @@
 package group.demo.controllers;
 
 public class ControllerHelper {
-    public static final String BASE_URL_v1 = "/api/v1";
+    public static final String API_BASE_URL_v1 = "/api/v1";
 }
