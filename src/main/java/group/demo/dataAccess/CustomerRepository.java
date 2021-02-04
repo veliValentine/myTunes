6a04d4f3ba03d68501ab4218c9b03c35aef2b076
veliValentine <nikkevanukas1995@gmail.com>
2021-02-04 14:28:07 +0200

    refactor: added helper methods

--- src/main/java/group/demo/dataAccess/CustomerRepository.java
+++ src/main/java/group/demo/dataAccess/CustomerRepository.java
@@ -12,12 +12,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CustomerRepository {
-    private final String URL = ConnectionHelper.CONNECTION_URL;
     private Connection connection = null;
 
     private final Logger logger;
 
-    private final String customerFields = "" +
+    private final String baseCustomerFields = "" +
             "CustomerId, " +
             "FirstName, " +
             "LastName, " +
@@ -33,21 +32,11 @@ public class CustomerRepository {
     public ArrayList<Customer> getCustomers() {
         ArrayList<Customer> customers = null;
         try {
-            // Open connection
-            connection = DriverManager.getConnection(URL);
-            logger.logToConsole("Connection to database opened");
-
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("select " + customerFields + "from Customer");
-
-            // Run statement
+            openConnectionAndLog();
+            PreparedStatement preparedStatement = prepareQuery("select " + baseCustomerFields + "from Customer;");
             ResultSet resultSet = preparedStatement.executeQuery();
-
             // Create new Customers list and add each customer to it
-            customers = new ArrayList<>();
-            while (resultSet.next()) {
-                customers.add(parseCustomerResultSet(resultSet));
-            }
+            customers = parseCustomersResultSet(resultSet);
             logger.logToConsole("\tgetCustomers successful");
         } catch (Exception e) {
             logger.errorToConsole(e.toString());
@@ -57,53 +46,31 @@ public class CustomerRepository {
         return customers;
     }
 
-    private Customer parseCustomerResultSet(ResultSet resultSet) {
-        Customer customer = null;
-        try {
-            customer = new Customer(
-                    resultSet.getString("customerId"),
-                    resultSet.getString("FirstName"),
-                    resultSet.getString("LastName"),
-                    resultSet.getString("Country"),
-                    resultSet.getString("PostalCode"),
-                    resultSet.getString("Phone"),
-                    resultSet.getString("Email")
-            );
-        } catch (Exception e) {
-            logger.errorToConsole(e.toString());
+    private ArrayList<Customer> parseCustomersResultSet(ResultSet resultSet) throws Exception {
+        ArrayList<Customer> customers = new ArrayList<>();
+        while (resultSet.next()) {
+            customers.add(parseBaseCustomerResultSet(resultSet));
         }
-        return customer;
+        return customers;
     }
 
     public Customer getCustomer(String customerId) {
         Customer customer = null;
         try {
-            // open connection
-            connection = DriverManager.getConnection(URL);
-            logger.logToConsole("Connection to database opened");
-
-            // Prepare statement
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("select " + customerFields + " from Customer where CustomerId = ?");
+            openConnectionAndLog();
+            PreparedStatement preparedStatement = prepareQuery("select " + baseCustomerFields + " from Customer where CustomerId = ?;");
             preparedStatement.setString(1, customerId);
-
             // Execute
             ResultSet resultSet = preparedStatement.executeQuery();
-
             // Parse data
             while (resultSet.next()) {
-                customer = parseCustomerResultSet(resultSet);
+                customer = parseBaseCustomerResultSet(resultSet);
             }
             logger.logToConsole("\tgetCustomer successful");
         } catch (Exception e) {
             logger.errorToConsole(e.toString());
         } finally {
-            try {
-                connection.close();
-                logger.logToConsole("Connection to database closed");
-            } catch (Exception e) {
-                logger.errorToConsole(e.toString());
-            }
+            finallyCloseConnectionAndLog();
         }
         return customer;
     }
@@ -111,44 +78,20 @@ public class CustomerRepository {
     public boolean addCustomer(Customer inputCustomer) {
         boolean success = false;
         try {
-            // Open connection
-            connection = DriverManager.getConnection(URL);
-            logger.logToConsole("Connection to database opened");
-
-            // Prepare statement
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("Insert Into Customer (FirstName, LastName, Country, PostalCode, Phone, Email) values (?,?,?,?,?,?)");
-
+            openConnectionAndLog();
+            PreparedStatement preparedStatement = prepareQuery("Insert Into Customer (FirstName, LastName, Country, PostalCode, Phone, Email) values (?,?,?,?,?,?);");
             setBasicCustomerValuesToPreparedStatement(preparedStatement, inputCustomer);
-
             // run statement and get result
             int result = preparedStatement.executeUpdate();
             success = (result != 0);
             logger.logToConsole("\taddCustomer successful: " + success);
+
         } catch (Exception e) {
             logger.errorToConsole(e.toString());
         } finally {
-            try {
-                connection.close();
-                logger.logToConsole("Connection to database closed");
-            } catch (Exception e) {
-                logger.errorToConsole(e.toString());
-            }
+            finallyCloseConnectionAndLog();
         }
         return success;
-    }
-
-    private void setBasicCustomerValuesToPreparedStatement(PreparedStatement preparedStatement, Customer customer) {
-        try {
-            preparedStatement.setString(1, customer.getFirstName());
-            preparedStatement.setString(2, customer.getLastName());
-            preparedStatement.setString(3, customer.getCountry());
-            preparedStatement.setString(4, customer.getPostalCode());
-            preparedStatement.setString(5, customer.getPhoneNumber());
-            preparedStatement.setString(6, customer.getEmail());
-        } catch (Exception e) {
-            logger.errorToConsole(e.toString());
-        }
     }
 
     public boolean updateCustomer(String inputId, Customer inputCustomer) {
@@ -158,30 +101,20 @@ public class CustomerRepository {
                 logger.errorToConsole("updateCustomer ID mismatch: path variable id different than customer.id");
                 return false;
             }
-            // Open connection
-            connection = DriverManager.getConnection(URL);
-            logger.logToConsole("Connection to database opened");
+            openConnectionAndLog();
 
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("Update Customer Set FirstName = ?, LastName = ?, Country = ?, PostalCode = ?, Phone = ?, Email = ? WHERE CustomerId = ?;");
-
+            PreparedStatement preparedStatement = prepareQuery("Update Customer Set FirstName = ?, LastName = ?, Country = ?, PostalCode = ?, Phone = ?, Email = ? WHERE CustomerId = ?;");
+            // set basic Customer inputs and customer ID
             setBasicCustomerValuesToPreparedStatement(preparedStatement, inputCustomer);
             preparedStatement.setString(7, inputCustomer.getId());
-
             // run statement and get result
             int result = preparedStatement.executeUpdate();
             success = (result != 0);
             logger.logToConsole("\tcustomer updated successful: " + success);
-
         } catch (Exception e) {
             logger.errorToConsole(e.toString());
         } finally {
-            try {
-                connection.close();
-                logger.logToConsole("Connection to database closed");
-            } catch (Exception e) {
-                logger.errorToConsole(e.toString());
-            }
+            finallyCloseConnectionAndLog();
         }
         return success;
     }
@@ -189,20 +122,11 @@ public class CustomerRepository {
     public Map<String, String> customersInCountry() {
         Map<String, String> countryMap = null;
         try {
-            connection = DriverManager.getConnection(URL);
-            logger.logToConsole("Connection to database opened");
-
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("Select Country, count(country) From Customer group by Country order by COUNT(Country) DESC;");
-
+            openConnectionAndLog();
+            PreparedStatement preparedStatement = prepareQuery("Select Country, count(country) From Customer group by Country order by COUNT(Country) DESC;");
             ResultSet resultSet = preparedStatement.executeQuery();
-            countryMap = new LinkedHashMap<>();
-            while (resultSet.next()) {
-                countryMap.put(
-                        resultSet.getString("Country"),
-                        resultSet.getString("count(country)")
-                );
-            }
+            // Parse resultSet to country->count Map
+            countryMap = parseResultToCountyCountLinkedHashMap(resultSet);
             logger.logToConsole("\tcustomersInCountry successful");
         } catch (Exception e) {
             logger.errorToConsole(e.toString());
@@ -210,6 +134,48 @@ public class CustomerRepository {
             finallyCloseConnectionAndLog();
         }
         return countryMap;
+    }
+
+    private LinkedHashMap<String, String> parseResultToCountyCountLinkedHashMap(ResultSet resultSet) throws Exception {
+        LinkedHashMap<String, String> countryMap = new LinkedHashMap<>();
+        while (resultSet.next()) {
+            countryMap.put(
+                    resultSet.getString("Country"),
+                    resultSet.getString("count(country)")
+            );
+        }
+        return countryMap;
+    }
+
+    // helper methods used more than once
+    private void openConnectionAndLog() throws Exception {
+        connection = DriverManager.getConnection(ConnectionHelper.CONNECTION_URL);
+        logger.logToConsole("Connection to database opened");
+    }
+
+    private PreparedStatement prepareQuery(String query) throws Exception {
+        return connection.prepareStatement(query);
+    }
+
+    private void setBasicCustomerValuesToPreparedStatement(PreparedStatement preparedStatement, Customer customer) throws Exception {
+        preparedStatement.setString(1, customer.getFirstName());
+        preparedStatement.setString(2, customer.getLastName());
+        preparedStatement.setString(3, customer.getCountry());
+        preparedStatement.setString(4, customer.getPostalCode());
+        preparedStatement.setString(5, customer.getPhoneNumber());
+        preparedStatement.setString(6, customer.getEmail());
+    }
+
+    private Customer parseBaseCustomerResultSet(ResultSet resultSet) throws Exception {
+        return new Customer(
+                resultSet.getString("customerId"),
+                resultSet.getString("FirstName"),
+                resultSet.getString("LastName"),
+                resultSet.getString("Country"),
+                resultSet.getString("PostalCode"),
+                resultSet.getString("Phone"),
+                resultSet.getString("Email")
+        );
     }
 
     private void finallyCloseConnectionAndLog() {
