2e5587445cf323d8ffa629c57e4d838001baa7b8
veliValentine <nikkevanukas1995@gmail.com>
2021-02-06 13:39:42 +0200

    feat: added String id

---
+++ src/main/java/group/demo/models/Song.java
@@ -0,0 +1,51 @@
+package group.demo.models;
+
+public class Song {
+    private String id;
+    private String trackName;
+    private String artist;
+    private String album;
+    private String genre;
+
+
+    // Getters and setters
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
